afe0c3b022acc1903e745fd67b43837bade2d0ee
Albert Jendrzejczak <aweczet@gmail.com>
2021-02-24 19:35:22 +0100

    add: Camera following player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform _target;
+
+    private void Start()
+    {
+        _target = GameObject.FindWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 cameraPosition = Vector3.Lerp(transform.position, new Vector3(_target.position.x, 0, transform.position.z), Time.fixedDeltaTime);
+        transform.position = cameraPosition;
+    }
+}
