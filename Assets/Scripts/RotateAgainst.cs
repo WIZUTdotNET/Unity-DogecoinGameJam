5b8f934089ee2a2648404f7c2cd78416f232ae30
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-02-25 16:34:59 +0100

    ✨ Add Rotation script, Add Pendulum script

---
+++ Assets/Scripts/RotateAgainst.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAgainst : MonoBehaviour
+{
+    public GameObject centerOfRotation;
+    public float speedOfRotation;
+    void Update()
+    {
+        transform.RotateAround(centerOfRotation.transform.position, -Vector3.forward, speedOfRotation * Time.deltaTime);
+    }
+}
