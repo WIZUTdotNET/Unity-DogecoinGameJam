c5f4fd546ee33a0d7d43cb28739d5002a5eaf435
mm44419 <mm44419@zut.edu.pl>
2021-02-25 13:16:17 +0100

    EndlessLvl

---
+++ Assets/Scripts/copyscenetest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copyscenetest : MonoBehaviour
+{
+    
+    public GameObject road;
+    void Start()
+    {
+        StartCoroutine(generateLevel());
+    }
+    public IEnumerator generateLevel()
+    {
+        yield return new WaitForSeconds(5f);
+        transform.position += new Vector3(21, 0);
+        Debug.Log("Text: ");
+        StartCoroutine(generateLevel());
+    }
+}
