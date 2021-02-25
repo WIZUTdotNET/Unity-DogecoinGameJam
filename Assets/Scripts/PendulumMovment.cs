b245f097dbb0c5333a7bde2a61dd2de5a19609c6
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-02-25 19:20:49 +0100

    :clown_face: Minor fixed

--- Assets/Scripts/PendulumMovment.cs
+++ Assets/Scripts/PendulumMovment.cs
@@ -9,20 +9,10 @@ public class PendulumMovment : MonoBehaviour
     public float speedOfPendulum;
     public float swingOffset;
     public float swingAngle;
-    public int directionOfSwing;
+    public int directionOfSwing = 1;
 
     private float _angleSum;
     private float _actualAngle;
-
-    private void Swing()
-    {
-        transform.RotateAround(centerOfRotation.transform.position, directionOfSwing * Vector3.forward,
-            _actualAngle);
-
-        _angleSum += directionOfSwing * _actualAngle;
-        if (math.abs(_angleSum + swingOffset) > swingAngle)
-            directionOfSwing *= -1;
-    }
 
     private void Update()
     {
@@ -35,5 +25,15 @@ public class PendulumMovment : MonoBehaviour
 
         if (_angleSum < -360)
             _angleSum += 360;
+    }
+
+    private void Swing()
+    {
+        transform.RotateAround(centerOfRotation.transform.position, directionOfSwing * Vector3.forward,
+            _actualAngle);
+
+        _angleSum += directionOfSwing * _actualAngle;
+        if (math.abs(_angleSum + swingOffset) > swingAngle)
+            directionOfSwing *= -1;
     }
 }
