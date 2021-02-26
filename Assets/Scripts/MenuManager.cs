7bbf921095631a42151d3e4a25d2bddbbc16b370
Albert Jendrzejczak <aweczet@gmail.com>
2021-02-26 18:32:14 +0100

    add: menu script

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("albert_scene");
+    }
+
+    public void Settings()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("albert_MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
