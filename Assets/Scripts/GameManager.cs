29cb3d3ca33dd64debedcac778fc6bae6ee736e3
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-02-26 16:46:39 +0100

    :clown_face: scene update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,24 +6,37 @@ using UnityEngine.UI;
 
 public class GameMenager : MonoBehaviour
 {
-    private static int _points;
+    private static int _coinPoints;
+    private static int _minionPoints;
+
     private static Transform _coinUI = GameObject.Find("Canvas/Coin").transform.GetChild(0);
 
     public static void GameEnd()
     {
         var currentSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(currentSceneName);
-        _points = 0;
+        _coinPoints = 0;
+        _minionPoints = 0;
     }
 
-    public static void AddPoint()
+    public static void InitializeCoinUi()
     {
-        _points++;
+        _coinUI = GameObject.Find("Canvas/Coin").transform.GetChild(0);
+    }
+
+    public static void AddCoinPoint()
+    {
+        _coinPoints++;
         UpdateCoinAmmount();
     }
-    
+
     private static void UpdateCoinAmmount()
     {
-        _coinUI.GetComponent<Text>().text = _points.ToString();
+        _coinUI.GetComponent<Text>().text = _coinPoints.ToString();
+    }
+
+    public static void AddMinionPoint()
+    {
+        _minionPoints++;
     }
 }
