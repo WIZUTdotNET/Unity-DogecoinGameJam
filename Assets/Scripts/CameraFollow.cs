af976f92fc4d0bcf7906d940fc4727df850bdcff
Albert Jendrzejczak <aweczet@gmail.com>
2021-02-26 17:57:23 +0100

    fix: minor changes to background and player/camera movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,7 @@
 public class CameraFollow : MonoBehaviour
 {
     private Transform _target;
-    private readonly float speed = 10f;
+    private float speed = 2f;
 
     private void Start()
     {
@@ -15,5 +15,6 @@ public class CameraFollow : MonoBehaviour
         var cameraPosition = Vector3.Lerp(transform.position, new Vector3(_target.position.x, 0, transform.position.z),
             speed * Time.fixedDeltaTime);
         transform.position = cameraPosition;
+        speed = _target.GetComponent<PlayerMovement>().speed;
     }
 }
