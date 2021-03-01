bce91e7251c88bcfb2c813955189c192f4e17eed
Albert Jendrzejczak <aweczet@gmail.com>
2021-03-01 20:29:00 +0100

    fix: popup

--- Assets/Scripts/WowPopup.cs
+++ Assets/Scripts/WowPopup.cs
@@ -7,7 +7,11 @@ public class WowPopup : MonoBehaviour
     private Transform _transformPosition;
     private float _timer;
     private Color _textColor;
-
+    private Color32[] _colors = new[] { new Color32(255, 0, 0, 255), new Color32(88, 245, 117, 255), 
+        new Color32(55, 161, 191, 255), new Color32(243, 7, 250, 255), new Color32(230, 228, 52, 255) };
+    private string[] _sayings = new[] { "wow", "much wow", "very wow", "such wow", "coin", "much coin", "very coin", "such coin", "cool"};
+    
+//, 
     public static WowPopup Create(Transform transformPosition, Transform prefab)
     {
         Transform wowPopupTransform = Instantiate(prefab, transformPosition.position, Quaternion.identity);
@@ -25,8 +29,9 @@ public class WowPopup : MonoBehaviour
     private void SetUp(Transform givenTransform)
     {
         _transformPosition = givenTransform;
+        _textMeshPro.faceColor = _colors[Random.Range(0, _colors.Length)];
         _textColor = _textMeshPro.color;
-        _textMeshPro.text = "wow";
+        _textMeshPro.text = _sayings[Random.Range(0, _sayings.Length)];
         _timer = .3f;
     }
 
