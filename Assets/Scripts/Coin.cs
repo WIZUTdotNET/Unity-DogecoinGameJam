2dd82377ece71cdb5294d0cbf2765fb984c2791d
Albert Jendrzejczak <aweczet@gmail.com>
2021-03-01 17:44:55 +0100

    add: popup after coin collection (prototype)

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,11 +2,13 @@
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private Transform wowPopup;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             GameManager.AddCoinPoint();
+            WowPopup.Create(collision.transform, wowPopup);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/WowPopup.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class WowPopup : MonoBehaviour
+{
+    private TextMeshPro _textMeshPro;
+    private Transform _transformPosition;
+    private float _timer;
+    private Color _textColor;
+
+    public static WowPopup Create(Transform transformPosition, Transform prefab)
+    {
+        Transform wowPopupTransform = Instantiate(prefab, transformPosition.position, Quaternion.identity);
+        WowPopup wowPopup = wowPopupTransform.GetComponent<WowPopup>();
+        wowPopup.SetUp(transformPosition);
+
+        return wowPopup;
+    }
+    private void Awake()
+    {
+        _textMeshPro = transform.GetComponent<TextMeshPro>();
+        SetUp(transform);
+    }
+
+    private void SetUp(Transform givenTransform)
+    {
+        _transformPosition = givenTransform;
+        _textColor = _textMeshPro.color;
+        _textMeshPro.text = "wow";
+        _timer = .3f;
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(_transformPosition.position.x, transform.position.y + 8f * Time.deltaTime, transform.position.z);
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
+        {
+            _textColor.a -= 3f * Time.deltaTime;
+            _textMeshPro.color = _textColor;
+            if (_textColor.a < 0)
+                Destroy(gameObject);
+        }
+    }
+}
